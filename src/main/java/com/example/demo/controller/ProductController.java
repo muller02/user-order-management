0d2020178189175b39713b2bb70c9cd309bf2445
daback93 <daback93@gmail.com>
2025-10-03 22:59:33 +0900

    feat: ProductController POST, GET 메소드 생성

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,5 +1,28 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ProductDTO;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+@RequestMapping("/api/v1/product")
 public class ProductController {
+
+    @PostMapping(value="/save")
+    public String saveProduct(@RequestBody ProductDTO productDTO) {
+        
+        return null;
+    }
+    
+    @GetMapping(value="/get")
+    public String getMethodName(@RequestParam String productId) {
+        return null;
+    }
     
 }
