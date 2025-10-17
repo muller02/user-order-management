863a2d234cee45ddaecbdc01e997044ebfff7d7c
daback93 <daback93@gmail.com>
2025-10-17 10:47:33 +0900

    chore: add @EnableJpaAuditing annotaion in DemoApplicaion.java for Auditing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// JPA Auditing 활성화 => @CreatedDate, @LastModifiedDate 어노테이션 사용 가능
+@EnableJpaAuditing
 @SpringBootApplication
 public class DemoApplication {
 
