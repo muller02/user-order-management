6ceb85f3a46d1d5d1c86f9846bb5265b5da2f6c2
daback93 <daback93@gmail.com>
2025-10-24 22:51:53 +0900

    refactor: delete ProductDTO, add ProductCreateDTO, ProductPatchDTO, ProductResponseDTO - ProductDTO 제거 및 생성, 부분수정, 응답 DTO 생성 - Product entity toDTO 메소드에 필드값 추가

--- src/main/java/com/example/demo/dto/ProductPatchDTO.java
+++ src/main/java/com/example/demo/dto/ProductPatchDTO.java
@@ -1,5 +1,6 @@
 package com.example.demo.dto;
 
-public record ProductPatchDTO(Long productId, Integer productStock) {
+public record ProductPatchDTO(Long id, 
+                            Integer stock) {
     
 }
---
+++ src/main/java/com/example/demo/dto/ProductResponseDTO.java
@@ -0,0 +1,30 @@
+package com.example.demo.dto;
+
+import com.example.demo.entity.Product;
+import com.example.demo.entity.ProductStatus;
+
+import lombok.Builder;
+
+@Builder
+public record ProductResponseDTO(Long id, 
+                        String name, 
+                        Integer price,
+                        String description, 
+                        Integer stock,
+                        String status,
+                        String createdBy,
+                        String createdAt,
+                        String updatedAt) {
+
+    // public Product toEntity() {
+    //     return Product.builder()
+    //             .id(this.id)
+    //             .name(this.name)
+    //             .price(this.price)
+    //             .description(this.description)
+    //             .stock(this.stock)
+    //             .status(ProductStatus.valueOf(this.status)) // Enum 변환
+    //             .createdBy() // User Entity 변환 필요 시
+    //             .build();
+    // }
+} 
